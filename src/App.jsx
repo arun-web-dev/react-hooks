2e25bd168c381e45df0be0a442a60e4fa0cd00b9
arun-web-dev <miuiandrew@gmail.com>
2022-03-24 12:09:56 +0530

    useref feature finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  /*
+  using use ref we can do below  
+  1.Dom reference - 
+  2.useful for storing previous state
+  3.hold mutable value prevent re-render of the component
 
+  */
+  const [name, setName] = useState("");
+  const [counter, setCounter] = useState(0);
+  const previousCounterRef = useRef("");
+  const inputEl = useRef("");
+
+  useEffect(() => {
+    previousCounterRef.current = counter;
+  }, [counter]);
+
+  const resetInput = () => {
+    setName("");
+    inputEl.current.focus();
+  };
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <div>
+          <input
+            ref={inputEl}
+            name="name"
+            autoComplete="off"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button onClick={resetInput}>Reset</button>
+          <div>My name is {name}</div>
+          <div>
+            <h1>Random Counter :{counter}</h1>
+            {typeof previousCounterRef.current !== "undefined" && (
+              <h2>Previous Counter :{previousCounterRef.current}</h2>
+            )}
+            <button onClick={(e) => setCounter(Math.ceil(Math.random() * 100))}>
+              Generate Number
+            </button>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
