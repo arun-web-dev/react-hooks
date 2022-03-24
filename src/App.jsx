3b54dc667f784007daa266951ce573bf9d1ea53b
arun-web-dev <miuiandrew@gmail.com>
2022-03-24 10:05:10 +0530

    useState Feature added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState("arun");
+  const [flag, setFlag] = useState(false);
+  const [steps, setSteps] = useState(0);
+  const [names, setNames] = useState([]);
+  function changeName() {
+    console.log("clicked");
+    return setFlag(!flag);
+  }
+  function increment() {
+    return setSteps(steps + 1);
+  }
+  function decrement() {
+    return setSteps(steps - 1);
+  }
 
+  function addNames(e) {
+    e.preventDefault();
+    if (e.target[0].value === "") return;
+    setNames([...names, { id: names.length, name: name }]);
+    setName("");
+  }
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <p> Use State hook string mainpulation</p>
+        <div>Hello {flag ? name : ""}</div>
+        <button onClick={changeName}>CLick me !</button>
+        <p> Use State hook numbers mainpulation</p>
+        <button onClick={increment}>+</button>
+        <div>{steps}</div>
+        <button onClick={decrement}>-</button>
+        <p> Use State hook Array and Object Mainpulation</p>
+        <form onSubmit={addNames}>
+          <input
+            type="text"
+            value={name}
+            placeholder="add names"
+            onChange={(e) => {
+              return setName(e.target.value);
+            }}
+          />
+          <button>submit</button>
+        </form>
+        <ul>
+          {names.map((item) => (
+            <li key={item.id}> {item.name} </li>
+          ))}
+        </ul>
+      </header>
     </div>
   );
 }
