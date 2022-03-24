b08c964de4d4490fed21d1afafa01b56d6a1f261
arun-web-dev <miuiandrew@gmail.com>
2022-03-24 18:17:43 +0530

    going to refer usememo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  /* 
+  1. Memoize the function (useCallback) vs Memoize the value (useMemo)
+  2.Referential equality for functions
+  */
+
+  const [counter, setCounter] = useState(1);
+
+  const result = useMemo(() => {
+    return factorial(counter);
+  }, [counter]);
 
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>
+          Factorial of {counter} is : <span>{result}</span>
+        </h1>
+        <div>
+          <button onClick={() => setCounter - 1}>Decremenet</button>
+          <button onClick={() => setCounter + 1}>Increment</button>
+        </div>
+      </header>
     </div>
   );
 }
